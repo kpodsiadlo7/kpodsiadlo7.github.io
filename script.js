44b13b5a41a6583c82e40a154c7a4373f066b4b8
Karol Podsiadlo <karolekk131@gmail.com>
2022-09-12 22:09:54 +0200

    fixing myself

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-karol.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://api.trello.com/1/members/karolkarol17';
+  const trelloApiRoot = 'https://kodilla-tasks-karol.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
