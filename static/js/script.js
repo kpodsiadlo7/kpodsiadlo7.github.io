5d6199071d73ca23336897ed6472928233a72a95
kpodsiadlo7 <karolekk131@gmail.com>
2024-01-24 16:25:59 +0100

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+//index.html
+function redirectToLol(){
+    window.location.href="/lolhome.html";
+}
+//end index.html
+
+//lolhome.html
+function redirectToSummoner(){
+    window.location.href="/summoner.html";
+}
+function redirectToLol(){
+    alert("TODO");
+    //TODO
+}
+//end lolhome.html
