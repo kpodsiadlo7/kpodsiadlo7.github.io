eceeb0d4229bf5180953f1a30217904afea3c90e
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2024-02-09 19:51:16 +0100

    fixed bug with championkills

--- static/js/popup.js
+++ static/js/popup.js
@@ -38,23 +38,11 @@ async function openPopup(event,matchId,puuId) {
 }
 
 function setObjectives(data) {
-    var championKillsForTeam100 = "" ;
-    var championKillsForTeam200 = "" ;
-
-    // first set championKills from enemy team
     for(var obj of data['teamObjective']) {
-        if(data.teamId === 100) {
-            championKillsForTeam200 = obj.championKills;
+        if(obj.teamId === 100) {
+            createObjective(obj);
         } else {
-            championKillsForTeam100 = obj.championKills;
-        }
-    }
-
-    for(var obj of data['teamObjective']) {
-        if(data.teamId === 100) {
-            createObjective(obj,championKillsForTeam100);
-        } else {
-            createObjective(obj,championKillsForTeam200);
+            createObjective(obj);
         }
     }
 }
@@ -85,7 +73,7 @@ function createObjective(obj,championKills) {
     `
     championDiv.innerHTML = `
         <div class="championText">Przeciwników: </div>
-        <div class="championKills">${championKills}</div>
+        <div class="championKills">${obj.championKills}</div>
     `
     winDiv.innerHTML = `
         <div id="win">${obj.teamId === 100 ? "Wygrana" : "Przegrana"}</div>
