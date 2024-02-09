20cf266df1b45bf45078e68a733c7cfeb3082f73
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2024-02-09 11:55:19 +0100

    add warmup for app

--- static/js/lolhome.js
+++ static/js/lolhome.js
@@ -4,3 +4,10 @@ function redirectToSummoner(){
 function redirectToMatches(){
     window.location.href="/matchinfo.html"
 }
+
+document.addEventListener('DOMContentLoaded', async function() {
+    fetch(`${window.home_url}`, {
+        headers: {
+          'ngrok-skip-browser-warning': 'true',
+        }})
+});
