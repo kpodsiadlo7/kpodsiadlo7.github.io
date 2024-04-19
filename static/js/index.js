6e031c25bd78c59527937cf5aea5ccd7831a5b19
kpodsiadlo7 <karolekk131@gmail.com>
2024-04-19 19:06:28 +0200

    add astronomy picture of the day

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,24 @@
 function redirectToLol() {
     window.location.href = "/lolhome.html";
 }
+
+document.addEventListener('DOMContentLoaded', async function() {
+    try {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`);
+        if (response.ok) {
+            const data = await response.json();
+            addPhotoWithData(data);
+        } else {
+            throw new Error('Wystąpił problem z pobieraniem danych.');
+        }
+    } catch (error) {
+        console.error('Wystąpił błąd:', error);
+    }
+});
+
+function addPhotoWithData({copyright, url, title, date}) {
+    document.getElementById('astronomyImg').src = url;
+    document.getElementById('title').innerText = 'Tytuł: '+title; 
+    document.getElementById('author').innerText = ' Autor: '+copyright; 
+    document.getElementById('date').innerText = date;
+}
