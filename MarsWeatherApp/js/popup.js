be9ec6ca66a3fe5a349a8db8ef1ee0b3151618e1
kpodsiadlo7 <karolekk131@gmail.com>
2024-06-01 23:51:37 +0200

    popup for future weather

---
+++ MarsWeatherApp/js/popup.js
@@ -0,0 +1,23 @@
+function openPopup() {
+
+    fetch('popup.html') 
+    .then(response => response.text()) 
+    .then(html => {
+        document.getElementById('popupContainer').innerHTML = html; 
+        document.getElementById('myPopup').style.display = 'block'; 
+    })
+    .catch(error => console.error('Wystąpił błąd podczas pobierania popupa:', error));
+}
+function closePopup() {
+    var popup = document.getElementById('myPopup');
+    var content = document.getElementById('content');
+    var blur = document.getElementById('backgroundColor');
+
+    content.style.animation = 'none';
+    content.style.animation = 'fadeOutPopup 0.3s ease forwards';
+    blur.style.animation = 'blurOut 0.3s ease forwards';
+
+    setTimeout(function() {
+        popup.style.display = 'none';
+    }, 300);
+}
