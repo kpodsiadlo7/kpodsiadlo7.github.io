e2b56b53192c5ef42928836c046579eed9e1bb3e
kpodsiadlo7 <karolekk131@gmail.com>
2024-06-09 11:46:28 +0200

    preparing for new application

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,11 @@
 function redirectToLol() {
-    window.location.href = "LeagueProfileSearch/lolhome.html";
+    window.location.href = '/LeagueProfileSearch/lolhome.html';
 }
 
-function redirectToMarsWeather(){
-    window.location.href='/MarsWeatherApp/mars.html';
+function redirectToMarsWeather() {
+    window.location.href = '/MarsWeatherApp/mars.html';
+}
+
+function redirectToFishingSnap() {
+    window.location.href = '/FishSnap/fishsnapMM.html';
 }
